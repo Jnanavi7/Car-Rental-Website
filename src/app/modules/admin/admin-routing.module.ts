fa37dec2da98a8071ec297c8ad25ce7ef7e9b163
Sangeeta-Gaddi <gaddisangeeta0@gmail.com>
2023-07-05 22:52:00 -0700

    h1

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from 'src/app/home/home.component';
 
 const routes: Routes = [
   {path:'',component:AdminDashboardComponent,
 children:[
   {path:'user',component:UserComponent},
+  {path:'home',component:HomeComponent},
   {path:'', redirectTo:'/admin/user', pathMatch:'full'}
 ],
 },
